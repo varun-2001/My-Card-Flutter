4a4ccf1e6ad8b1890a315d3e75068f92bce2acb7
varun-2001 <57247403+varun-2001@users.noreply.github.com>
2020-08-03 04:00:14 +0530

    Itsa me

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,83 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage('images/icon.jpg'),
+            ),
+            Text(
+              'Varun Suvarna',
+              style: TextStyle(
+                  fontSize: 40.0,
+                  fontFamily: 'Pacifico',
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            ),
+            Text(
+              'FLUTTER DEVELOPER',
+              style: TextStyle(
+                  fontSize: 20.0,
+                  fontFamily: 'SourceSansPro',
+                  color: Colors.teal[100],
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 20.0,
+              width: 150.0,
+              child: Divider(
+                thickness: 1.5,
+                color: Colors.teal.shade100,
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.phone,
+                  color: Colors.teal,
+                ),
+                title: Text(
+                  '+919844283881',
+                  style: TextStyle(
+                      fontSize: 17.0,
+                      color: Colors.teal.shade900,
+                      fontFamily: 'Source Sans Pro'),
+                ),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.email,
+                  color: Colors.teal,
+                ),
+                title: Text(
+                  'varunnsuvarna2001@gmail.com',
+                  style: TextStyle(
+                      fontSize: 17.0,
+                      color: Colors.teal.shade900,
+                      fontFamily: 'Source Sans Pro'),
+                ),
+              ),
+            ),
+          ]),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
